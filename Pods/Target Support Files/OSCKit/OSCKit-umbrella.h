c866b7883bf780076cf5fb6892f54cad91f6471b
soh_la <soh_la@yahoo.com>
2017-05-24 01:31:06 +0800

    pod added

---
+++ Pods/Target Support Files/OSCKit/OSCKit-umbrella.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+#import "OSCClient.h"
+#import "OSCKit.h"
+#import "OSCMessage.h"
+#import "OSCProtocol.h"
+#import "OSCServer.h"
+
+FOUNDATION_EXPORT double OSCKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char OSCKitVersionString[];
+
