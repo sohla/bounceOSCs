c940e71e457c00a401efb87f5d8810e8c55dbdd2
soh_la <soh_la@yahoo.com>
2017-11-23 17:35:41 +1100

    basic osc server listening

--- bounceOSCs/AppDelegate.swift
+++ bounceOSCs/AppDelegate.swift
@@ -7,21 +7,28 @@
 //
 
 import UIKit
+import OSCKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate ,OSCServerDelegate{
 
     var window: UIWindow?
+    let server: OSCServer = OSCServer.init()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         let _:SensorTransmitters = SensorTransmitters()
+        server.delegate = self
+        server.listen(9001)
         
         return true
     }
-
+    func handle(_ message: OSCMessage!) {
+        
+        print([message.address,message.arguments])
+    }
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
